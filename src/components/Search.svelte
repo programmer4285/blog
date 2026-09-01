ce5bea4d64dcb929c9e3c9bbb205e1d61cd895e3
eloquent <woodmanjd@pm.me>
2026-08-31 21:55:13 -0400

    finish search

--- src/components/Search.svelte
+++ src/components/Search.svelte
@@ -1,24 +1,86 @@
 <script lang="ts">
   import icon from "../assets/search.svg";
-  import Fuse from "fuse.js";
+  import Fuse, { type FuseResult } from "fuse.js";
+
+  interface Post {
+    id: string;
+    title: string;
+    description: string;
+    tags: string[];
+    body: string | undefined;
+  }
+
+  type Snippet =
+    | {
+        type: "match";
+        before: string;
+        highlight: string;
+        after: string;
+      }
+    | { type: "more"; count: number };
 
   interface Props {
-    index: {
-      title: string;
-      description: string;
-      tags: string[];
-      body: string | undefined;
-    }[];
+    index: Post[];
   }
+
+  function buildSnippets(result: FuseResult<Post>): Snippet[] {
+    const MAX_SIZE = 25;
+
+    let snippets: Snippet[] = [];
+    for (const match of result.matches ?? []) {
+      if (match.key === "title") continue;
+
+      if (!match.value) continue;
+
+      for (const ind of match.indices) {
+        const start = Math.max(0, ind[0] - MAX_SIZE);
+        const end = Math.min(match.value.length, ind[1] + MAX_SIZE);
+        snippets.push({
+          type: "match",
+          before: match.value.substring(start, ind[0]),
+          highlight: match.value.substring(ind[0], ind[1] + 1),
+          after: match.value.substring(ind[1] + 1, end),
+        });
+      }
+    }
+
+    if (snippets.length > 4) {
+      const count = snippets.length - 4;
+      snippets = snippets.slice(0, 4);
+      snippets.push({ type: "more", count });
+    }
+
+    return snippets;
+  }
+
   let { index }: Props = $props();
 
-  const fuse = $derived(
+  let fuse = $derived(
     new Fuse(index, {
-      keys: ["title", "description", "tags", "body"],
+      keys: [
+        {
+          name: "title",
+          weight: 3,
+        },
+        {
+          name: "description",
+          weight: 2,
+        },
+        {
+          name: "body",
+          weight: 1,
+        },
+      ],
+      ignoreLocation: true,
+      includeMatches: true,
+      minMatchCharLength: 2,
+      threshold: 0.25,
     }),
   );
 
   let query = $state("");
+
+  let results = $derived(query.trim() ? fuse.search(query, { limit: 5 }) : []);
 </script>
 
 <div>
@@ -35,6 +97,40 @@
         if (e.newState === "closed") query = "";
       }}
     />
+
+    {#if results.length > 0}
+      <ul class="search-results">
+        {#each results as result (result.item.id)}
+          <li>
+            <article>
+              <h3>
+                <a href={`/posts/${result.item.id}`}>{result.item.title}</a>
+              </h3>
+              <ul class="snippets">
+                {#each buildSnippets(result) as snippet, i (i)}
+                  {#if snippet.type === "match"}
+                    <li>
+                      <small
+                        >{snippet.before}<strong>{snippet.highlight}</strong
+                        >{snippet.after}</small
+                      >
+                    </li>
+                  {:else}
+                    <li class="more-matches">
+                      <small
+                        ><a href={`/posts/${result.item.id}`}
+                          >{snippet.count} more matches →</a
+                        ></small
+                      >
+                    </li>
+                  {/if}
+                {/each}
+              </ul>
+            </article>
+          </li>
+        {/each}
+      </ul>
+    {/if}
   </div>
 </div>
 
@@ -51,7 +147,7 @@
     margin-top: 0.5rem;
     inset: auto;
     position-area: bottom span-left;
-    width: 20rem;
+    width: 25rem;
     max-width: 75vw;
   }
 
@@ -59,5 +155,37 @@
     border: none;
     background: var(--pico-background-color);
     overflow: visible;
+  }
+
+  .search-results {
+    display: flex;
+    flex-direction: column;
+    padding: 0;
+    list-style: none;
+    overflow-y: auto;
+    margin-top: 0.5rem;
+    height: 18rem;
+  }
+
+  .search-results > li {
+    padding: 0;
+    width: 90%;
+  }
+
+  .snippets {
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+  }
+  .snippets > li {
+    list-style: none;
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .more-matches {
+    margin-top: 0.5rem;
+    border-top: solid;
   }
 </style>
