0a124340251dcd33e75131031f3efe8626162de0
eloquent <woodmanjd@pm.me>
2026-09-02 19:57:32 -0400

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,18 @@ import { defineConfig } from "astro/config";
 
 import svelte from "@astrojs/svelte";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  site: "https://therefactoredlife.com",
+  integrations: [svelte(), sitemap()],
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "catppuccin-latte",
+        dark: "catppuccin-mocha",
+      },
+    },
+  },
 });
